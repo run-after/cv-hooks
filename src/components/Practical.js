ca317ebc063d876ecfda177870908363fddd4746
run-after <run.after@protonmail.com>
2020-11-16 10:37:49 -0600

    Practical job listings allow edit

--- src/components/Practical.js
+++ src/components/Practical.js
@@ -1,62 +1,63 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 const Practical = () => {
-
   const [practical, setPractical] = useState({
+    
     'job1': {
       title: 'Job 1',
       location: 'Location 1',
       dates: '1/19 - Current',
       description: 'Job 1 description - What have you done. Why was it great. What were the challenges'
-    },
+    
+    }, 
     'job2': {
       title: 'Job 2',
       location: 'Location 2',
       dates: '1/19 - Current',
       description: 'Job 2 description - What have you done. Why was it great. What were the challenges'
     }
+    
   });
-  
-  
-    const handleChange = (e) => {
-      e.preventDefault();
-      setPractical((prevState) => ({
-        ...prevState,
-        [e.target.id]: {
-          title: e.target[0].value,
-          location: e.target[1].value,
-          dates: e.target[2].value,
-          description: e.target[3].value
-        }
-      }));
-      hidePracticalModal(e);
-    };
+    
+  const handleChange = (e) => {
+    e.preventDefault();
+    setPractical((prevState) => ({
+      ...prevState,
+      [e.target.dataset.job]: {
+        title: e.target[0].value,
+        location: e.target[1].value,
+        dates: e.target[2].value,
+        description: e.target[3].value
+      }
+    }));
+    hidePracticalModal(e);
+  };
 
   const showPracticalModal = (e) => {
-    const job = e.target.parentNode;
-    job.classList.add('d-none');
-    const practicalModal = document.querySelector('#prac-mod-'+job.id);
+    const jobListing = e.target.parentNode;
+    jobListing.classList.add('d-none');
+    const practicalModal = document.querySelector('#prac-mod-' + jobListing.id);
     practicalModal.classList.remove('d-none');
     practicalModal.classList.add('d-block');
   };
 
   const hidePracticalModal = (e) => {
-    e.target.classList.remove('d-block');
-    e.target.classList.add('d-none');
+    const practicalModal = e.target;
+    practicalModal.classList.remove('d-block');
+    practicalModal.classList.add('d-none');
+
+    const jobListing = document.getElementById(practicalModal.dataset.job)
+    jobListing.classList.remove('d-none');
   }
 
-
-  // Working... Need to clean up the 2nd return below
-  // then use useEffect to change the state of job
-
   return (
-    <div>{/*1*/}
-      <div id='practical' className='container'>{/*2*/}
+    <div>
+      <div id='practical' className='container'>
         <h3 className='title text-center'>Experience</h3>
         
         {Object.keys(practical).map((key) => {
           const job = practical[key];
-          return (//This might be a function (displayJobsInState)?
+          return (
             
             <div key={key}>
               <div className='row'>
@@ -68,7 +69,7 @@ const Practical = () => {
                   <div className='description'>{job.description}</div>
                 </div>
               </div>
-              <form onSubmit={handleChange } id={'prac-mod-'+key} className='practical-modal d-none'>
+              <form onSubmit={handleChange } data-job={key} id={'prac-mod-'+key} className='practical-modal d-none'>
                 <input className='col-12' placeholder='title' defaultValue={job.title} /><br/>
                 <input className='col-12' placeholder='location' defaultValue={job.location} /><br/>
                 <input className='col-12' placeholder='dates' defaultValue={job.dates} /><br/>
@@ -78,13 +79,15 @@ const Practical = () => {
             </div>
           )
         })}
-      </div>{/*2 */}
+      </div>
       
       
-    </div>//1
+    </div>
         
   )
   
 }
 
 export default Practical;
+
+//useEffect is everything that's not user-interaction based like API requests on load
